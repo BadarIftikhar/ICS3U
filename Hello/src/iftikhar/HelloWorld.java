555d14599b94e349126379e5864cd2da11db5947
Badar Iftikhar <324011360@C-H6304V1.tdsb.on.ca>
2017-02-21 14:45:45 -0500

    Pushing the Hello World program

---
+++ Hello/src/iftikhar/HelloWorld.java
@@ -0,0 +1,18 @@
+package iftikhar;
+/**
+ * 
+ * Hello World
+ * Prints "Hello World"
+ * Feb. 21, 2017
+ * @author Badar Iftikhar
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+
+	}
+
+}
